5a64f0c792f4048261bbd5660d3505f5b00b40db
cehui0303 <cehui0303@163.com>
2019-11-29 22:38:26 +0800

    修改访问许可网站； 采用10秒测试定时触发； 判断异步加载元素按钮是否存在监测

--- maotai.js
+++ maotai.js
@@ -4,23 +4,81 @@ var timer = null;
 
 //检测状态
 function checkElementState(path,callback){
+	var ele = document.querySelector(path);
+	if(ele){
+		callback && callback();
+	}else{
+		console.log('异步加载元素中....' + path );
+		setTimeout( function(){checkElementState(path,callback);},200);
+	}
 }
 
 
 
 //点击购买按钮
 function clickBuy(){
+	
 	console.log('买！');
+	
+	//票的数量  如果还不可以购买，这个地方获取会失败 
+	var amount = document.getElementsByClassName('mui-amount-increase')[0];
+	amount && amount.click();  //+1
+	
+	var btnBuy = document.querySelector('');
+	
 }
 
 
+//结算
+function checkOut(){
+	
+	
+	console.log('结算开始....');
+	var btn = document.getElementById('J_Go');
+	
+	if(btn){
+		btn.click();
+	}else{
+		console.log('结算按钮没找到');
+	}
+	
+}
+
+function checkOutAsync(){
+	checkElementState('#J_Go',checkOut);
+}
+
+//提交订单
+function submitOrder(){
+	
+	console.log('提交订单开始....');
+	
+	
+	
+	checkElementState('.go-btn',function(){
+		var btn = document.querySelector(".go-btn");
+	
+		if(btn){
+			btn.click();
+		}else{
+			console.log('提交订单按钮没找到');
+		}
+			
+	});
+}
+
+
+
+//目标时间
+	var dDate = new Date();
+	//dDate.setHours(21,39,0);
+	dDate.setSeconds( dDate.getSeconds() + 10 );
+	
 //进入时间判断循环
 function enterTimeCheckLoop(callback){
 	var date = new Date();
 	
-	//目标时间
-	var dDate = new Date();
-	dDate.setHours(16,50,0);
+	
 	
 	
 	var diff = Date.parse(dDate) - Date.parse(date) ;
@@ -36,7 +94,7 @@ function enterTimeCheckLoop(callback){
 	}else{
 		setTimeout(function(){ enterTimeCheckLoop(callback);},500);
 		
-		console.log('--');
+		//console.log('--');
 	}
 	
 	
@@ -48,8 +106,21 @@ function enterTimeCheckLoop(callback){
 function main(){
 	console.log('############################开始抢购茅台############################');
 	
-	//进入时间判断
-	enterTimeCheckLoop( clickBuy );
+	//debugger;
+	
+	var href = window.location.href;
+	if(href.indexOf('cart.tmall.com') > -1 ){
+		//结算页面
+		
+		//进入时间判断
+		enterTimeCheckLoop( checkOutAsync );
+	
+	
+	}else if(href.indexOf('buy.tmall.com') > -1 ){
+		//提交订单页面
+		
+		submitOrder();
+	}
 	
 }
 
