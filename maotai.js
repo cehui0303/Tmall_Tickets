5638bbe47181018214962a83489972f077de7cc2
cehui0303 <cehui0303@163.com>
2020-01-08 10:45:44 +0800

    根据天猫超市的抢购规则，修改为晚上八点开始抢

--- maotai.js
+++ maotai.js
@@ -70,11 +70,11 @@ function submitOrder(){
 
 
 //目标时间
-	var dDate = new Date();  //10点和22点开抢
+	var dDate = new Date();  //10点和20点开抢
 	if( dDate.getHours() < 10 ){
 		dDate.setHours(9,59,59.2);
 	}else{
-		dDate.setHours(21,59,59.2);
+		dDate.setHours(19,59,59.2);
 	}
 	
 	//dDate.setSeconds( dDate.getSeconds() + 10 );
